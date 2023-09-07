c5badf745b9d47c0269ae66c8394a35068729095
Seungjunlee0 <Seungjunlee15@gmail.com>
2023-09-07 16:06:41 +0900

    fixed some of things

--- onetool.py
+++ onetool.py
@@ -2,7 +2,7 @@ import os, glob
 
 width_title = ["0_80em","0_90em","1_00em","1_10em","1_20em","1_30em", "1_32em", "1_40em", "1_50em","1_60em","1_70em","1_80em"]
 #                 0        1        2         3       4       5         6        7         8         9       10        11
-width  = width_title[1]
+width  = width_title[10]
 
 
 
@@ -10,7 +10,7 @@ all_folder = glob.glob('/u/user/seungjun/SE_UserHome/lhe/'+width+'/*.lhe')
 all_file = [x for x in all_folder if os.path.isfile(x)]
 for file_name in all_file:
     file_new = file_name[-7:-4].replace(" ","")
-    output = " /u/user/seungjun/SE_UserHome/root/0_90em/lhe_"+file_new+".root "
-    command_line="cmsRun lhe_to_edm.py "+file_name+" /u/user/seungjun/SE_UserHome/root/0_90em/lhe_"+file_new+".root "
+    output = " /u/user/seungjun/SE_UserHome/root/"+width+"/lhe_"+file_new+".root "
+    command_line="cmsRun lhe_to_edm.py "+file_name+" /u/user/seungjun/SE_UserHome/root/"+width+"/lhe_"+file_new+".root "
     print(command_line)
     os.system(command_line)
