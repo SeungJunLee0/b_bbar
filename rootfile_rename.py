3004eca519fd51be004d855786185c897ce19882
Seungjunlee0 <Seungjunlee15@gmail.com>
2023-09-09 14:15:42 +0900

    just commit for remove it

--- rootfile_rename.py
+++ rootfile_rename.py
@@ -2,7 +2,7 @@ import os, glob
 
 width_title = ["0_80em","0_90em","1_00em","1_10em","1_20em","1_30em", "1_32em", "1_40em", "1_50em","1_60em","1_70em","1_80em"]
 #                 0        1        2         3       4       5         6        7         8         9       10        11
-width  = width_title[1]
+width  = width_title[10]
 
 print(width)
 print(width)
